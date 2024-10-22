42ac277f80ced1f0e2c185351315a2c4936334ef
georg-stone <george@thingbomb.com>
2024-10-22 17:23:35 -0400

    add settings

--- src/src/App.jsx
+++ src/src/App.jsx
@@ -2,6 +2,20 @@ import { useState, useEffect } from "react";
 import { CommandPalette } from "./components/ui/cmd";
 import CharacterCounter from "./CharacterCounter";
 import WordCounter from "./WordCounter";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./components/ui/dropdown-menu";
+import { DropdownMenuLabel } from "@radix-ui/react-dropdown-menu";
+import { SettingsIcon } from "lucide-react";
+import { Button } from "./components/ui/button";
+import { Sun } from "lucide-react";
+import { Moon } from "lucide-react";
+import { useTheme } from "./components/ui/theme-provider";
+import { Computer } from "lucide-react";
 
 const images = [
   { url: "assets/photos/1.jpg", color: "#FFFFFF" },
@@ -36,6 +50,7 @@ function App() {
   const [time, setTime] = useState(new Date());
   const [selectedImage, setSelectedImage] = useState({});
   const [selectedPage, setSelectedPage] = useState("none");
+  const { setTheme } = useTheme();
 
   useEffect(() => {
     const intervalId = setInterval(() => setTime(new Date()), 1000);
@@ -50,13 +65,13 @@ function App() {
 
   return (
     <div
-      className="flex flex-col items-center justify-center h-screen bg-black text-white bg-cover"
+      className="flex flex-col items-center justify-center h-screen bg-white dark:bg-black text-black dark:text-white bg-cover"
       style={{ backgroundImage: `url(${selectedImage.url})` }}
       id="app"
     >
       <CommandPalette setSelectedPage={setSelectedPage} />
       <h1
-        className="text-7xl font-bold clock"
+        className="text-7xl font-bold clock select-none"
         style={{ color: selectedImage.color }}
       >
         {time.toLocaleTimeString(undefined, options)}
@@ -67,6 +82,29 @@ function App() {
       {selectedPage === "word-counter" && (
         <WordCounter setSelectedPage={setSelectedPage} />
       )}
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild className="fixed bottom-0 left-0 z-50">
+          <Button variant="outline" aria-label="Settings" size="icon">
+            <SettingsIcon className="h-5 w-5" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuLabel>Themes</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={() => setTheme("dark")}>
+            <Moon />
+            <span>Dark theme</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("light")}>
+            <Sun />
+            <span>Light theme</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("system")}>
+            <Computer />
+            <span>System default</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 }
