fcfdef7bb629e089380d2c18e86073d91297e975
Daihecyy <paul.saubusse@gmail.com>
2025-01-06 23:55:34 +0100

    First working version

---
+++ apps/new-tab/src/hooks/clockFormatter.ts
@@ -0,0 +1,38 @@
+import { createSignal, onMount } from "solid-js";
+import { createStoredSignal } from "./localStorage";
+
+function formattedClock() {
+  const [clockFormat, setClockFormat] = createStoredSignal("clockFormat", "");
+  const [currentClock, setCurrentClock] = createSignal(createClock(new Date()));
+  function createClock(date: Date) {
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    const formattedHours = hours % 12 || 12;
+    if (clockFormat() == "12h") {
+      return {
+        time: `${formattedHours}:${formattedMinutes}`,
+        amPm: "",
+      };
+    } else if (clockFormat() == "24h") {
+      return {
+        time: `${hours}:${formattedMinutes}`,
+        amPm: "",
+      };
+    } else {
+      return {
+        time: `${formattedHours}:${formattedMinutes}`,
+        amPm: hours >= 12 ? "PM" : "AM",
+      };
+    }
+  }
+  onMount(() => {
+    setInterval(() => {
+      setCurrentClock(createClock(new Date()));
+    }, 1000);
+  });
+
+  return currentClock;
+}
+
+export { formattedClock };
